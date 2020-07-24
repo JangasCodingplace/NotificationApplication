ab4dd97f7e30ecf57b958b5ed3c58942055b5e8f
Janis <janisgoesser92@gmail.com>
2020-07-24 17:46:36 +0200

    Transport Notificationcount to context

--- Notification/views.py
+++ Notification/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
 
+from .models import Notification
+
 def index(request):
-    return render(request, 'dashboard/index.html')
+    if not request.user.is_authenticated:
+        return render(request, 'dashboard/notAuth.html')
+    unreaded_notification_count = Notification.objects.filter(assigned_to=request.user, is_read=False).count()
+
+    context = {
+        'unreaded_notification_count':unreaded_notification_count
+    }
+
+    return render(request, 'dashboard/index.html', context=context)

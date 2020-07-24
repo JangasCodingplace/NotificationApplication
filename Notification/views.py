a5e256822b76652eda5e0008d74ff762cf8e2853
Janis <janisgoesser92@gmail.com>
2020-07-24 18:21:49 +0200

    Http get count, get unreaded, get readed notifications for a user

--- Notification/views.py
+++ Notification/views.py
@@ -5,10 +5,14 @@ from .models import Notification
 def index(request):
     if not request.user.is_authenticated:
         return render(request, 'dashboard/notAuth.html')
-    unreaded_notification_count = Notification.objects.filter(assigned_to=request.user, is_read=False).count()
+    notifications = request.user.notifications_assigned_to_user.order_by('-creation_date')
+    old_notifications = notifications.filter(is_read=True)
+    unreaded_notifications = notifications.filter(is_read=False).order_by('creation_date')
 
     context = {
-        'unreaded_notification_count':unreaded_notification_count
+        'unreaded_notification_count':unreaded_notifications.count(),
+        'unreaded_notifications':unreaded_notifications[:5],
+        'old_notifications':old_notifications[:3]
     }
 
     return render(request, 'dashboard/index.html', context=context)

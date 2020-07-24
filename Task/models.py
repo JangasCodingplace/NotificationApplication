725fca0438b38689d2a652947a7de6bff5b7bfcc
Janis <janisgoesser92@gmail.com>
2020-07-24 14:57:37 +0200

    Create Task App + Model

---
+++ Task/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.conf import settings
+
+class Task(models.Model):
+    # assigned to user
+    assigned_to = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="tasks_assigned_to_user"
+    )
+    # created by user
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="tasks_created_by_user"
+    )
+    # task title
+    title = models.CharField(
+        max_length=60
+    )
+    # task discription
+    body = models.TextField(
+        blank=True
+    )
+    # done information
+    is_done = models.BooleanField(
+        default=False
+    )
+    # creation date
+    creation_date = models.DateTimeField(
+        auto_now_add=True
+    )

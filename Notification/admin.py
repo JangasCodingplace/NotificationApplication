c2b824bf1278d9cdcd6c6a852936eee63c672539
Janis <janisgoesser92@gmail.com>
2020-07-24 15:22:59 +0200

    Create Notification Admin

--- Notification/admin.py
+++ Notification/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Notification
 
-# Register your models here.
+class NotificationAdmin(admin.ModelAdmin):
+    model = Notification
+    list_display = ('__str__','is_read','group',)
+    list_filter = ('is_read','group',)
+    search_fields = ('assigned_to__username',)
+
+admin.site.register(Notification, NotificationAdmin)
--- Task/admin.py
+++ Task/admin.py
@@ -3,7 +3,7 @@ from .models import Task
 
 class TaskAdmin(admin.ModelAdmin):
     model = Task
-    list_display = ('__str__','created_by','is_done','creation_date')
+    list_display = ('__str__','created_by','is_done','creation_date',)
     list_filter = ('is_done',)
     search_fields = ('created_by__username', 'assigned_to__username',)
 
